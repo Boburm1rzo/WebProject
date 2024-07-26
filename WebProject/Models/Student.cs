3573092ab192c8abf98ab584be831eeb117bc07d
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-27 03:51:08 +0500

    updates in Models Folder

--- WebProject/Models/Student.cs
+++ WebProject/Models/Student.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Grade { get; set; }
+        public double Grade { get; set; }
     }
 }
