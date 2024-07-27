47754d367af3e6a6d55e87559a942f3bbcba962b
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-27 22:55:11 +0500

    Exceprions folder in Domain

---
+++ University.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace University.Domain.Exceptions;
+
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException() : base() { }
+    public EntityNotFoundException(string message) : base(message) { }
+    public EntityNotFoundException(string message, Exception inner) : base(message, inner) { }
+}
