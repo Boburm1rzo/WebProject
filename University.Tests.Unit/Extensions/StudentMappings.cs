b2022b204ea634a7a71d368195a97726daa55086
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-04 16:59:50 +0500

    added Unit test

---
+++ University.Tests.Unit/Extensions/StudentMappings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Extensions;
+using University.ViewModels.Student;
+
+namespace University.Tests.Unit.Extensions;
+
+public class StudentMappings
+{
+    [Fact]
+    public void ShouldThrowException_WhenToEntityCalled_WithNull()
+    {
+        StudentCreateView view = null!;
+
+        Assert.Throws<ArgumentNullException>(() => view!.ToEntity());
+    }
+
+    [Fact]
+    public void ShouldCorrectly_MapToEntity()
+    {
+        StudentCreateView view = new StudentCreateView 
+        { 
+            FirstName = "John", 
+            LastName = "Doe", 
+            Email = "john@gmail.com", 
+            PhoneNumber = "+998914045014" 
+        };
+        var entity = view.ToEntity();
+
+        Assert.Equal("John", entity.FirstName);
+        Assert.Equal("Doe", entity.LastName);
+        Assert.Equal("john@gmail.com", entity.Email);
+        Assert.Equal("+998914045014", entity.PhoneNumber);
+    }
+}
