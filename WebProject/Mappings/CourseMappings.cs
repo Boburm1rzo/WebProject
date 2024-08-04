401c9bb08328e14d32024c00e28a8ffcec0971c7
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-04 17:02:38 +0500

    mappings folder

---
+++ WebProject/Mappings/CourseMappings.cs
@@ -0,0 +1,56 @@
+﻿using University.Domain.Entities;
+using University.ViewModels.Course;
+
+namespace University.Mappings;
+
+public static class CourseMappings
+{
+    public static CourseView ToView(this Course course)
+    {
+        return new CourseView
+        {
+            Id = course.Id,
+            Name = course.Name,
+            Description = course.Description,
+            Discount = course.Discount,
+            NumberOfModules = course.NumberOfModules,
+            Price = course.Price,
+            Rating = course.Rating,
+        };
+    }
+
+    public static UpdateCourseView ToUpdateView(this Course course)
+    {
+        return new UpdateCourseView
+        {
+            Name = course.Name,
+            Description = course.Description,
+            Discount = course.Discount,
+            NumberOfModules = course.NumberOfModules,
+            Price = course.Price,
+            Rating = course.Rating,
+        };
+    }
+
+    public static Course ToEntity(this CreateCourseView view)
+    {
+        return new Course
+        {
+            Name = view.Name,
+            Description = view.Description,
+            Discount = view.Discount,
+            NumberOfModules = view.NumberOfModules,
+            Price = view.Price,
+            Rating = view.Rating,
+        };
+    }
+
+    public static Course ToEntity(this UpdateCourseView view)
+    {
+        var entity = (view as CreateCourseView).ToEntity();
+        entity.Id = view.Id;
+
+        return entity;
+    }
+
+}
---
+++ WebProject/Mappings/StudentMappings.cs
@@ -0,0 +1,52 @@
+﻿using University.Domain.Entities;
+using University.ViewModels.Student;
+
+namespace University.Mappings;
+
+public static class StudentMappings
+{
+    public static Student ToEntity(this StudentCreateView student)
+    {
+        ArgumentNullException.ThrowIfNull(student);
+
+        return new Student
+        {
+            Email = student.Email,
+            FirstName = student.FirstName,
+            LastName = student.LastName,
+            PhoneNumber = student.PhoneNumber,
+        };
+    }
+
+    public static Student ToEntity(this StudentUpdateView view)
+    {
+        var entity = (view as StudentCreateView).ToEntity();
+        entity.Id = view.Id;
+
+        return entity;
+    }
+
+    public static StudentView ToView(this Student student)
+    {
+        return new StudentView
+        {
+            Id = student.Id,
+            FirstName = student.FirstName,
+            LastName = student.LastName,
+            Email = student.Email,
+            PhoneNumber = student.PhoneNumber,
+        };
+    }
+
+    public static StudentUpdateView ToUpdateView(this Student student)
+    {
+        return new StudentUpdateView
+        {
+            Id = student.Id,
+            FirstName = student.FirstName,
+            LastName = student.LastName,
+            Email = student.Email,
+            PhoneNumber = student.PhoneNumber,
+        };
+    }
+}
